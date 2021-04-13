46dc6d4595db6f014502965432e6746901e217d4
SuperPox <calebkohl@gmail.com>
2021-04-13 14:03:51 -0600

    commit tree 36f125bad6d82c6bd47fa14f5e3b9fd8c2900001

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+
+// function writeCards( nameArray, adjective ) {
+//     let cards = []
+//     for ( let i = 0; i < nameArray.length; i++) {
+//         cards.push( 'Thank you, ${nameArray[i]}, for the wonderful ${adjective} gift!' )
+//     }
+//     return cards
+// }
+
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
+
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num -= 1;
+    }
+}
+
+
